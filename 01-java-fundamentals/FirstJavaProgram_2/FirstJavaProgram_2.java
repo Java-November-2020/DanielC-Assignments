5d4d2d537f6652b6170a0fde8d29e714f47ff9dc
Dajocoh <danieljcoh@gmail.com>
2020-11-01 16:36:37 -0500

    first commit - and firstjavaprogram

---
+++ 01-java-fundamentals/FirstJavaProgram_2/FirstJavaProgram_2.java
@@ -0,0 +1,14 @@
+public class FirstJavaProgram_2{
+
+    // Entry Point
+    public static void main (String[] args) {
+
+        String name = "Daniel Cohen";
+        int age = 27;
+        String hometown = "Manorville, NY";
+
+        System.out.println("May name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
