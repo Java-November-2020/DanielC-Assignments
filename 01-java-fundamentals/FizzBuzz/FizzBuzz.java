ffb6e3548869971e5113a2a967ae2b6c2ccf92bb
Dajocoh <danieljcoh@gmail.com>
2020-11-03 11:29:38 -0500

    FizzBuzz assignment

---
+++ 01-java-fundamentals/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+
+public class FizzBuzz{
+    public static void fizzBuzz(int number){
+        for(int i = 0; i <= number; i++) {
+            if(i % 5 == 0 && i % 3 == 0){
+                System.out.println("FizzBuzz");
+            }   else if(i % 5 == 0){
+                System.out.println("Fizz");
+            } else if (i % 3 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
