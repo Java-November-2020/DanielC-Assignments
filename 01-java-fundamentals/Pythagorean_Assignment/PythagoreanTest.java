563b8c067a185c5c231da6f2a9642c961359f901
Dajocoh <danieljcoh@gmail.com>
2020-11-11 11:08:24 -0500

    puzzling assignment and whole 01-java file

--- 01-java-fundamentals/Pythagorean_Assignment/PythagoreanTest.java
+++ 01-java-fundamentals/Pythagorean_Assignment/PythagoreanTest.java
@@ -1,8 +1,7 @@
 
 public class PythagoreanTest {
     public static void main(String[] args) {
-        Pythagorean triangleC = new Pythagorean();
-        double TriangleHypo = triangleC.calculateHypotenuse(4,5);
-        System.out.println(TriangleHypo);
+        System.out.println(Pythagorean.calculateHypotenuse(4,5));
     }
 }
+
