32d7dc2b062a083e2e035efb6e1d6ccce27ea412
Dajocoh <danieljcoh@gmail.com>
2020-11-12 11:56:45 -0500

    hashmatique assignment

---
+++ 01-java-fundamentals/HashMap/HashMap.java
@@ -0,0 +1,23 @@
+/*
+import java.util.Set;
+
+public class HashMap {
+
+    public static void hashMapTrackList(){
+        HashMap<String, String> trackList = new HashMap<String, String>();
+
+        trackList.put("The evil that men do", "Livin' on a razors edge!");
+        trackList.put("Norweigan Wood", "Isn't it good Norweigan Wood?");
+        trackList.put("Don't give hate a chance", "Don't be givin' hate a chance!");
+        trackList.put("Le Sud", "...");
+
+        String songName = trackList.get("Le Sud");
+
+        Set<String> keys = trackList.keySet();
+        for(String key : keys) {
+            System.out.println(key);
+            System.out.println(trackList.get(key)); 
+        }
+    }
+}
+*/
