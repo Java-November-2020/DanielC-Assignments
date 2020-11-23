3403932537313cef5b5137c4be6672e5e0f738d8
Dajocoh <danieljcoh@gmail.com>
2020-11-23 12:41:30 -0500

    Zoo keeper part 1

---
+++ 02-java-oop/ZooKeeperPartOne/ZooKeeper/src/GorillaTest.java
@@ -0,0 +1,18 @@
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gorilla g = new Gorilla();
+		
+		g.throwSomething();
+		g.throwSomething();
+		g.throwSomething();
+		g.eatBananas();
+		g.eatBananas();
+		g.climb();
+		
+
+	}
+
+}
